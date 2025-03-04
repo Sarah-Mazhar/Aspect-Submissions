a4b7a9c8b9411349e5636844bd323681930dff36
Sarah-Mazhar <147240333+Sarah-Mazhar@users.noreply.github.com>
2025-03-04 02:03:37 +0200

    lab1-http methods done

---
+++ lab1/src/main/java/com/example/demo/controller/UnifiedController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/data")
+public class UnifiedController {
+
+    @GetMapping
+    public String getData() {
+        return "GET response";
+    }
+
+    @PostMapping
+    public String postData() {
+        return "POST response";
+    }
+
+    @PutMapping
+    public String putData() {
+        return "PUT response";
+    }
+
+    @DeleteMapping
+    public String deleteData() {
+        return "DELETE response";
+    }
+
+    // Inner static aspect class
+    @Aspect
+    @Component
+    public static class LoggingAspect {
+
+        @Before("execution(* com.example.demo.controller.UnifiedController.*(..))")
+        public void logBefore(JoinPoint joinPoint) {
+            System.out.println(">>> Aspect Triggered Before Method: " + joinPoint.getSignature().getName());
+        }
+    }
+}
